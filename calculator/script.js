2776a1e1fbe6cbbd720fe794ca35036b6ac16b67
Haktan Öker <82652469+haktanoker@users.noreply.github.com>
2023-12-26 17:08:24 +0300

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,105 +1,100 @@
 var result = document.querySelector(".result");
 
-const ac = document.querySelector('.AC');
-const de = document.querySelector('.DE');
-const point = document.querySelector('.point');
-const division = document.querySelector('.division');
+const ac = document.querySelector(".AC");
+const de = document.querySelector(".DE");
+const point = document.querySelector(".point");
+const division = document.querySelector(".division");
 
-const seven = document.querySelector('.seven');
-const eight = document.querySelector('.eight');
-const nine = document.querySelector('.nine');
-const multi = document.querySelector('.multi');
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const multi = document.querySelector(".multi");
 
-const four = document.querySelector('.four');
-const five = document.querySelector('.five');
-const six = document.querySelector('.six');
-const sub = document.querySelector('.sub');
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const sub = document.querySelector(".sub");
 
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
-const plus = document.querySelector('.plus');
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const plus = document.querySelector(".plus");
 
-const dbl_zero = document.querySelector('.dbl_zero');
-const zero = document.querySelector('.zero');
-const equals = document.querySelector('.equals');
+const dbl_zero = document.querySelector(".dbl_zero");
+const zero = document.querySelector(".zero");
+const equals = document.querySelector(".equals");
 
-// Tuşa basıldığında karşılık gelen butonu bulmak için bir harita oluştur
 const keyToButtonMap = {
-    '0': zero,
-    '1': one,
-    '2': two,
-    '3': three,
-    '4': four,
-    '5': five,
-    '6': six,
-    '7': seven,
-    '8': eight,
-    '9': nine,
-    '+': plus,
-    '-': sub,
-    '*': multi,
-    '/': division,
-    ',': point,
-    '.': point,
-    'Enter': equals
-};
+    '0' : zero,
+    '1' : one,
+    '2' : two,
+    '3' : three,
+    '4' : four,
+    '5' : five,
+    '6' : six,
+    '7' : seven,
+    '8' : eight,
+    '9' : nine,
+    '+' : plus,
+    '-' : sub,
+    '*' : multi,
+    '/' : division,
+    ',' : point,
+    '.' : point,
+    'Enter' : equals,
+}
 
-// Klavye olaylarını dinle
-window.addEventListener('keydown', function (e) {
-    if (result.innerText === "Hata") {
+window.addEventListener('keydown', function(e) {
+    if(result.innerText === "Hata"){
         result.innerText = "";
     }
-    if (e.key === 'Backspace') {
+    if(e.key === 'Backspace'){
         e.preventDefault();
         removeLastChar();
     } else if (e.key === 'Enter') {
         e.preventDefault();
-        evaluateResult();
+        evaulateResult();
     } else if (keyToButtonMap.hasOwnProperty(e.key)) {
         e.preventDefault();
         keyToButtonMap[e.key].click();
+        console.log(keyToButtonMap[e.key])
     }
-});
+})
 
-equals.addEventListener('click', evaluateResult);
+equals.addEventListener('click', evaulateResult);
 
-// Buton tıklama olaylarını dinle
 document.querySelectorAll('.btn').forEach(button => {
     button.addEventListener('click', (e) => {
         let lastChar = result.innerText.slice(-1);
         let value = e.target.textContent;
 
-        if (result.innerText === "Hata") {
+        if(result.innerText === "Hata"){
             result.innerText = "";
-        } else if (['+', '-', '*', '/', '.', ''].includes(value) &&
-            ['+', '-', '*', '/', '.', ''].includes(lastChar)) {
-        } else if (value === "=") {
-            evaluateResult();
-        } else if (value === "AC") {
-            result.innerText = "";
-        } else if (value === "DE") {
+        }else if (['+', '-', '*', '/', '.', ' '].includes(value) && ['+', '-', '*', '/', '.', ' '].includes(lastChar)){} 
+        else if (value === "=") {
+            evaulateResult();
+        } else if(value === "AC") {
+            result.innerText = ""
+        } else if(value === "DE") {
             removeLastChar();
-        } else {
+        } else{
             result.innerText += value;
         }
-    });
-});
+    })
+})
 
-// Son karakteri silme fonksiyonu
 function removeLastChar() {
     result.innerText = result.innerText.slice(0, -1);
 }
 
-// Sonucu hesaplama ve gösterme fonksiyonu
-function evaluateResult() {
-    let operation = result.innerText.replace(/,/g, '.');
+function evaulateResult() {
+    let operation = result.innerText.replace(/,/g,'.');
 
     try {
         const resultValue = new Function('return ' + operation)();
         result.innerText = resultValue.toString().replace(/\./g, ',');
     } catch (e) {
         result.innerText = 'Hata';
-        console.error('Hatalı işlem:', e);
+        console.error('Hatalı işlem: ', e)
     }
 }
