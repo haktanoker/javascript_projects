e1ec2ec35b06285078b9015eaec45c88fd402797
Haktan Öker <82652469+haktanoker@users.noreply.github.com>
2024-01-04 22:43:10 +0300

    Update script.js

--- category todolist/script.js
+++ category todolist/script.js
@@ -7,44 +7,45 @@ const business = document.querySelector("#businessTasks");
 const other = document.querySelector("#otherTasks");
 
 function addTask() {
-    if (task.value === "" || task.value === " ") {
+    if (task.value === "" || task.value === " "){
         error.style.opacity = "1";
         task.value = "";
-    } else {
+    } else{
         let taskName = `
-        <div class="task">
-            <p>${task.value}</p>
-            <div class="deleteAndCheck">
-                <i class="far fa-trash-alt"></i>
-                <i class="fas fa-check"></i>
+            <div class="task">
+                <p>${task.value}</p>
+                <div class="deleteAndCheck">
+                    <i class="far fa-trash-alt"></i>
+                    <i class="fas fa-check"></i>
+                </div>
             </div>
-        </div>`
+        `;
         error.style.opacity = "0";
-        if (taskCategory.value == "personal") {
+        if (taskCategory.value == "personal"){
             personal.insertAdjacentHTML("beforeend", taskName);
-        }
-        if (taskCategory.value == "work") {
+        };
+        if (taskCategory.value == "work"){
             business.insertAdjacentHTML("beforeend", taskName);
-        }
-        if (taskCategory.value == "other") {
+        };
+        if (taskCategory.value == "other"){
             other.insertAdjacentHTML("beforeend", taskName);
-        }
+        };
         task.value = "";
         task.focus();
     }
 };
-// Aşağıdaki kod, sayfada neye basılırsa basılsın tetikler ve basılan şeyin içerisinde fa-trash-alt class'ı var mı diye kontrol eder
-document.addEventListener('click', function (e) {
-  if (e.target.classList.contains('fa-trash-alt')) {
-    e.target.parentElement.parentElement.style.display = "none";
-  } else if (e.target.classList.contains('fa-check')) {
-      let decoration = e.target.parentElement.parentElement.querySelector("p");
-      if (decoration.style.textDecoration == "line-through") {
-          decoration.style.textDecoration = "none";
-          decoration.style.color = "white";
-      } else {
-          decoration.style.textDecoration = "line-through";
-          decoration.style.color = "#aaaaab";
-      }
+
+document.addEventListener("click", function(e){
+    if (e.target.classList.contains('fa-trash-alt')){
+        e.target.closest(".task").remove();
+    } else if (e.target.classList.contains('fa-check')){
+        let decoration = e.target.closest(".task").querySelector("p");
+        if (decoration.style.textDecoration == "line-through"){
+            decoration.style.textDecoration = "none";
+            decoration.style.color = "white";
+        } else{
+            decoration.style.textDecoration = "line-through";
+            decoration.style.color = "#aaaaab";
+        }
     }
 });
