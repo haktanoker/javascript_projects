c92c057d87a04401f3e13ef1074d9528f3f4f648
Haktan Öker <82652469+haktanoker@users.noreply.github.com>
2024-01-06 23:50:05 +0300

    Update script.js

--- color_picker/script.js
+++ color_picker/script.js
@@ -4,7 +4,6 @@ const hex = document.querySelector("#hex");
 const rgb = document.querySelector("#rgb");
 const input = document.querySelector("#colorEnter");
 
-// Hex to Rgb
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
@@ -14,35 +13,31 @@ function hexToRgb(hex) {
     } : null;
 }
 
-// Sayfa ilk açıldığında bg rengini picker ile aynı yapmak
 var setColor = () => colorBG.style.backgroundColor = colorPicker.value;
 setColor();
 
-// Type içerisindeki renk kodlarını güncelleme
-function changeInputs(value) {
+function changeInputs(value){
     // hex
     hex.innerText = value;
     // rgb
-    let RGB = hexToRgb(value)
-    rgb.innerText = `rgb(${RGB.r}, ${RGB.g}, ${RGB.b})`
+    let RGB = hexToRgb(value);
+    rgb.innerText = `rgb(${RGB.r},${RGB.g},${RGB.b})`;
 }
 
-// Renk değiştikçe BG'yi değiştirmek
+// Renk değiştirmeyle eşzamanlı olarak bg'yi değiştirmek
 colorPicker.addEventListener("input", (e) => {
     colorBG.style.backgroundColor = e.target.value;
-    changeInputs(e.target.value)
-})
+    changeInputs(e.target.value);
+});
 
-// İnput'a renk kodu girilince oun çevirmek
+// inputa girilen kodu çevirmek
 input.addEventListener("input", (e) => {
     let inputValue = e.target.value;
-    if (inputValue[0] == "#") {
+    if(inputValue[0] == "#"){
         let convert = hexToRgb(inputValue);
-        colorBG.style.backgroundColor = `rgb(${convert.r}, ${convert.g}, ${convert.b})`
+        colorBG.style.backgroundColor = `rgb(${convert.r},${convert.g},${convert.b})`;
         changeInputs(inputValue);
         colorPicker.value = inputValue;
-    } else if (inputValue[0] == "r" || inputValue[0] == "R") {
-
     }
 })
 
